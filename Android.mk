825d821f4bac79b8761a3775b8db5b8c8a19d922
yyftxws <1040280380@qq.com>
2017-11-07 16:56:49 +0800

    init btconfig v1.0

---
+++ Android.mk
@@ -0,0 +1,22 @@
+#ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+BDROID_DIR:= system/bt
+
+LOCAL_C_INCLUDES := $(BDROID_DIR)/hci/include
+
+LOCAL_SRC_FILES:= \
+              btconfig.c
+
+LOCAL_MULTILIB := 32
+LOCAL_MODULE_TAGS := debug optional
+LOCAL_MODULE :=btconfig
+
+LOCAL_SHARED_LIBRARIES += libcutils   \
+                          libutils    \
+                          libdl
+
+include $(BUILD_EXECUTABLE)
+#endif
